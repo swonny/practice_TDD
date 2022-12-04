3622f2b48ebd4de693dcb38d999722d5ea29ce74
swonny <wonny921@gmail.com>
2022-12-04 16:57:22 +0900

    feat: TDD 테스트를 이용한 Account 클래스 생성

---
+++ src/main/java/account/Account.java
@@ -0,0 +1,21 @@
+package account;
+
+public class Account {
+    private int balance;
+
+    public Account(int money) {
+        this.balance = money;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int money) {
+        this.balance += money;
+    }
+
+    public void withdraw(int money) {
+        this.balance -= money;
+    }
+}
---
+++ src/test/java/account/AccountTest.java
@@ -0,0 +1,36 @@
+package account;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class AccountTest {
+
+    private Account account;
+
+    @Test
+    public void testAccount() {
+        account = new Account(10000);
+    }
+
+    @Test
+    public void testGetBalance() throws Exception {
+        assertThat(account.getBalance()).isEqualTo(10000);
+
+        assertThat(account.getBalance()).isEqualTo(1000);
+
+        assertThat(account.getBalance()).isEqualTo(0);
+    }
+
+    @Test
+    public void testDeposit() throws Exception {
+        account.deposit(1000);
+        assertThat(account.getBalance()).isEqualTo(11000);
+    }
+
+    @Test
+    public void testWithdraw() throws Exception {
+        account.withdraw(1000);
+        assertThat(account.getBalance()).isEqualTo(9000);
+    }
+}
